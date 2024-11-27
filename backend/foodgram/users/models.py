b3f76d4eab634285e4aec8087d6719d21d356bec
Dmitry Alexandrov <alexandrov.dm91@gmail.com>
2024-11-27 07:23:57 +0300

    Make Usermodel

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    """Модель пользователя."""
+    email = models.EmailField(
+        'Адрес электронной почты',
+        max_length=254,
+    )
+    username = models.CharField(
+        'Имя пользователя',
+        max_length=150,
+        unique=True
+    )
+    first_name = models.CharField(
+        'Имя',
+        max_length=150
+    )
+    last_name = models.CharField(
+        'Фамилия',
+        max_length=150
+    )
+
+    def __str__(self):
+        return self.username
+
+
