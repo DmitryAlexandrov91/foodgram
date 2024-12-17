867487fd931994eebde425168261d5c6d29f8892
Dmitry Alexandrov <alexandrov.dm91@gmail.com>
2024-12-17 11:21:36 +0300

    recipe collections test

--- backend/foodgram/loading_ingredients.py
+++ backend/foodgram/loading_ingredients.py
@@ -1,13 +1,17 @@
-"""Example to migrate data to db."""
+"""Скрипт записи ингредиентов в базу данных."""
 import csv
 import sqlite3
+
+from django.conf import settings
 
 
 con = sqlite3.connect("db.sqlite3")
 
 cur = con.cursor()
 
-with open('ingredients.csv', encoding='utf-8') as file:
+file_path = 'C:\\Users\\AlDmAl\\Desktop\\Python\\VSC\\Dev\\Final_projects_for_review\\foodgram\\data\\ingredients.csv'
+
+with open(file_path, encoding='utf-8') as file:
     data = csv.DictReader(file)
     to_base = [(i['name'], i['measurement_unit']) for i in data]
     for i in to_base:
