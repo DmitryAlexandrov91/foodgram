2f294725aee517b6c1b999b9725ef4514251be2f
Dmitry Alexandrov <alexandrov.dm91@gmail.com>
2024-12-24 14:55:19 +0300

    few api tests added

---
+++ backend/foodgram/api/tests.py
@@ -0,0 +1,22 @@
+"""Тесты приложения api."""
+from http import HTTPStatus
+
+from django.test import Client, TestCase
+
+
+class FoodgramAPITestCase(TestCase):
+    """Класс проверки api foodgram на получение списка рецептов и юзеров."""
+
+    def setUp(self):
+        """Создаёт атрибут класса guest_client."""
+        self.guest_client = Client()
+
+    def test_recipes_list(self):
+        """Проверка доступности списка рецептов."""
+        response = self.guest_client.get('/api/recipes/')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_task_creation(self):
+        """Проверка доступности списка юзеров."""
+        response = self.guest_client.get('/api/users/')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
